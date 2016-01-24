9336b23f604140dcd71917d659f270cb1a4c00e5
root <root@localhost.localdomain>
2016-01-24 19:56:35 +0530

    all new

---
+++ koken/storage/configuration/database.php
@@ -0,0 +1,9 @@
+<?php
+	return array(
+		'hostname' => 'localhost',
+		'database' => 'Koken',
+		'username' => 'root',
+		'password' => '123',
+		'prefix' => 'koken_',
+		'socket' => ''
+	);
---
+++ koken/storage/configuration/key.php
@@ -0,0 +1,6 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+// Do not edit or remove this file unless advised by Koken support
+// support@koken.me
+
+return '639b843bcaf0def71db568bb7e9479fc';
